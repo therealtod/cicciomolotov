d4a50f0af95b61fade6fccd03e784c97a249d1d5
Giunta Eugenio <e.giunta@reply.it>
2020-05-02 17:07:35 +0200

    add list and router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React from 'react';
+import { ListGroup } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="itemList">
+          <div className="headerList"> Item List </div>
+          <div className="List">
+            <ListGroup>
+              <ListGroup.Item><Link to="/1">Item 1</Link></ListGroup.Item>
+              <ListGroup.Item><Link to="/2">Item 2</Link></ListGroup.Item>
+              <ListGroup.Item><Link to="/3">Item 3</Link></ListGroup.Item>
+            </ListGroup>
+          </div>
+        </div>
+        <div className="itemDetail">
+          sdadas
+      </div>
+      </div>
+      <Switch>
+        <Route path="/1">
+          <About />
+        </Route>
+        <Route path="/2">
+          <Topics />
+        </Route>
+        <Route path="/3">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
